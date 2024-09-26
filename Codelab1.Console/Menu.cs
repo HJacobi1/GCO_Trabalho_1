5a0af59247832a8e43547b08befaf7fe4f80395a
HJacobi1 <henrique.jacobi1@gmail.com>
2024-09-25 22:33:29 -0300

    #1 OMG Menu

---
+++ Codelab1.Console/Menu.cs
@@ -0,0 +1,43 @@
+﻿using Codelab1.Console;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrabalhoUm.Console
+{
+    public static class Menu
+    {
+        public static string EscreverMenuString()
+        {
+            var menu = "-------- Utils Project --------" +
+                       " 1 - Abrir calculadora " +
+                       " 2 - Abrir navegador " +
+                       " 3 - Abrir Bloco de notas" +
+                       " 4 - Sair ";
+            return menu;
+        }
+        public static void ExecutarAcao(int acao)
+        {
+            switch (acao)
+            {
+                case 1:
+                    ExternalCaller.RunCalculator();
+                    break;
+
+                case 2:
+                    ExternalCaller.RunBrowser("https://yandex.com/?");
+                    break;
+
+                case 3:
+                    //To do
+                    break;
+
+                case 4:
+                    Environment.Exit(0);
+                    break;
+            }
+        }
+    }
+}
